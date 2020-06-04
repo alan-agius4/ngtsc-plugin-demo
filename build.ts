67043cf461880710a3f137f2954bf8d7d267327f
Alan Agius <alan.agius4@gmail.com>
2020-06-04 10:49:00 +0200

    remove incremental

--- build.ts
+++ build.ts
@@ -1,7 +1,6 @@
 import * as ts from 'typescript';
 import { NgTscPlugin, readConfiguration } from '@angular/compiler-cli';
 import { resolve } from 'path';
-import { createHash } from 'crypto';
 
 const config = readConfiguration(resolve('tsconfig.json'))
 const compilerOptions = config.options;
@@ -10,38 +9,29 @@ console.time('NgTscPlugin')
 const ngtsc = new NgTscPlugin(compilerOptions)
 console.timeEnd('NgTscPlugin')
 
-console.time('createIncrementalCompilerHost')
-const host = ts.createIncrementalCompilerHost(compilerOptions);
-console.timeEnd('createIncrementalCompilerHost')
+const host = ts.createCompilerHost(compilerOptions);
 
 // UnifiedModeHost is required in wrapHost type but optional in ngtsc CompilerHost
 //const wrappedHost = host;
 const wrappedHost = ngtsc.wrapHost(host as any, config.rootNames, compilerOptions)
 
-augmentHostWithVersioning(wrappedHost)
-
-console.time('createIncrementalProgram')
-//const oldProgram = ngtsc.getNextProgram();
-const program = ts.createIncrementalProgram({
+const program = ts.createProgram({
     options: compilerOptions,
     rootNames: config.rootNames,
     host: wrappedHost,
 });
-
-console.timeEnd('createIncrementalProgram')
 console.time('setupCompilation')
-ngtsc.setupCompilation(program.getProgram(), undefined);
+const { ignoreForEmit } = ngtsc.setupCompilation(program, undefined);
 console.timeEnd('setupCompilation')
 
-const builder = program
 console.time('diagnostics')
 const diagnostics = [
     ...ngtsc.getDiagnostics(),
     ...ngtsc.getOptionDiagnostics(),
-    ...builder.getOptionsDiagnostics(),
-    ...builder.getGlobalDiagnostics(),
-    ...builder.getSyntacticDiagnostics(),
-    ...builder.getSemanticDiagnostics(),
+    ...program.getOptionsDiagnostics(),
+    ...program.getGlobalDiagnostics(),
+    ...program.getSyntacticDiagnostics(),
+    ...program.getSemanticDiagnostics(),
 ];
 
 const formatHost: ts.FormatDiagnosticsHost = {
@@ -53,17 +43,5 @@ console.warn(diagnostics.map(d => ts.formatDiagnostic(d, formatHost)).join('\n')
 console.timeEnd('diagnostics')
 
 console.time('emit')
-builder.emit(undefined, undefined, undefined, undefined, ngtsc.compiler.prepareEmit().transformers)
+program.emit(undefined, undefined, undefined, undefined, ngtsc.compiler.prepareEmit().transformers)
 console.timeEnd('emit')
-
-function augmentHostWithVersioning(host: ts.CompilerHost): void {
-    const baseGetSourceFile = host.getSourceFile;
-    host.getSourceFile = function (...parameters) {
-        const file = baseGetSourceFile.call(host, ...parameters) as any;
-        if (file && file.version === undefined) {
-            file.version = createHash('sha256').update(file.text).digest('hex');
-        }
-
-        return file;
-    };
-}
